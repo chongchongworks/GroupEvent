ff87527dde626d1cefd014688d57a7f04777d15e
Home-PC <Home-PC@CHONGCHONG>
2017-07-14 22:20:52 +1000

    rest

--- EventMvc/Global.asax.cs
+++ EventMvc/Global.asax.cs
@@ -9,7 +9,7 @@ using System.Web.Routing;
 
 namespace EventMvc
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : System.Web.HttpApplication 
     {
         protected void Application_Start()
         {
